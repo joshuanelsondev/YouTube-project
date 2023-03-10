cb548065fc3d29f22d8f740e25a7b43691c6f8cb
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-10 12:15:52 -0500

    Adds missing curly brace

--- youtube-project/src/App.js
+++ youtube-project/src/App.js
@@ -1,10 +1,8 @@
-
 import Nav from './components/Nav';
 import Home from './components/Home';
 import About from './components/About';
 import Video from './components/Video';
 import VideoIndex from './components/VideoIndex';
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
@@ -13,11 +11,12 @@ export default function App() {
       <Router>
         <Nav />
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/videos" element={<VideoIndex/>} />
-          <Route path="/videos/:id" element={<Video/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/videos" element={<VideoIndex />} />
+          <Route path="/videos/:id" element={<Video />} />
         </Routes>
       </Router>
     </div>
   );
+}
