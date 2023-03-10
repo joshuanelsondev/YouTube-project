20407f88d5f0cf2ad337b7225f8cf86b8289d5c1
joshuanelsondev <joshuanelsondev@gmail.com>
2023-03-10 13:54:17 -0500

    Place .env file in root directory and create getVideos function in fetch.js

--- youtube-project/src/api/fetch.js
+++ youtube-project/src/api/fetch.js
@@ -1,5 +1,5 @@
-const Base_URL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults="
-const URL = process.env.REACT_APP_API_KEY
+const BASE_URL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 
 
@@ -7,7 +7,7 @@ const URL = process.env.REACT_APP_API_KEY
 
 // When searching for videos use the <snippet.channelId> after 'relatedToVideoId=' from the JSON: BASE_URL + 'part=snippet&relatedToVideoId=Ks-_Mh1QhMc&type=video&key=[YOUR_API_KEY]'
 
-export function getVideos() {
-    
+export function getVideos(videoQuantity, keyword) {
+    return fetch(`${BASE_URL}${videoQuantity}&q=${keyword}&key=${API_KEY}`).then(response => response.json());
 }
 
