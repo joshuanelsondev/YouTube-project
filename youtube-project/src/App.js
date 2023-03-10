ee8906b46788800fe7ac8cfdf378cad88a9296d4
joshuanelsondev <joshuanelsondev@gmail.com>
2023-03-10 14:28:27 -0500

    Import getVideos into Video.js

--- youtube-project/src/App.js
+++ youtube-project/src/App.js
@@ -3,11 +3,11 @@ import Home from './components/Home';
 import About from './components/About/About';
 import Video from './components/Video';
 import VideoIndex from './components/VideoIndex';
-import './App.css';
+// import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { getVideos } from './api/fetch';
 
-console.log(getVideos(25, "Ariana").then(response => console.log(response)))
+
+
 
 export default function App() {
   return (
