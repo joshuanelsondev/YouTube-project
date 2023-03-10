d2a8bb96b69566a0ccd6e6c47aec93a6cac827eb
SangUn Park <Sangunpark1@gmail.com>
2023-03-10 12:31:15 -0500

    need fetch calls to proceed

--- youtube-project/src/components/Home.js
+++ youtube-project/src/components/Home.js
@@ -1,3 +1,16 @@
-export default function Home() {
+// import { useState } from "react";
 
+export default function Home() {
+    // const [error, setError ] = useState(false)
+    // const [videos, setVideos ] = useState([])
+
+    // function handleSearch(event){
+
+    // };
+    
+    return (
+        <>
+        <input type="text" id="searchInput" /><input type="submit" />
+        </>
+        );
 };
