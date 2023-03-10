9befacb6c53576a4fd60249ba59f22270991c80d
SangUn Park <Sangunpark1@gmail.com>
2023-03-10 11:26:39 -0500

    components added, router almost completed?

--- youtube-project/src/App.js
+++ youtube-project/src/App.js
@@ -1,4 +1,8 @@
 import Nav from './components/Nav';
+import Home from './components/Home';
+import About from './components/About';
+import Video from './components/Video';
+import VideoIndex from './components/VideoIndex';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -8,7 +12,10 @@ function App() {
       <Router>
         <Nav />
         <Routes>
-          <Route/>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/videos" element={<VideoIndex/>} />
+          <Route path="/videos/:id" element={<Video/>} />
         </Routes>
       </Router>
     </div>
