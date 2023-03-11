0081db754d185225479b10811506445052a4b0d7
joshuanelsondev <joshuanelsondev@gmail.com>
2023-03-11 12:01:26 -0500

    Create state variables and functions for the search input

--- youtube-project/src/components/Home/Home.js
+++ youtube-project/src/components/Home/Home.js
@@ -1,18 +1,34 @@
 import './Home.css';
-// import { useState } from "react";
+import { useState } from "react";
+import { getVideos } from '../../api/fetch';
+import {v1 as generateUniqueID} from "uuid";
 
 export default function Home() {
   // const [error, setError ] = useState(false)
   // const [videos, setVideos ] = useState([])
 
-  // function handleSearch(event){};
+  const [userSearchInput, setUserSearchInput] = useState("");
+  const [searchHistory, setSearchHistory] = useState([]);
+
+  function handleTextInput(event) {
+    setUserSearchInput(event.target.value)
+  }
+
+  function handleSearch(event){
+    event.preventDefault();
+    const duplicateSearch = searchHistory.find(input => (input.search).toLowerCase() === userSearchInput.toLowerCase())
+    if (!duplicateSearch) {
+      setSearchHistory([...searchHistory, {search: userSearchInput, id: generateUniqueID()}]);
+    }
+
+  };
 
   return (
     <>
       <main>
         <div className="search-bar">
-          <input type="text" id="searchInput" />
-          <input type="submit" />
+          <input onChange={handleTextInput} type="text" id="searchInput" />
+          <input onClick={handleSearch} type="submit" />
         </div>
         <div>{/* Initial thumbnails/map over api data will go here */}</div>
       </main>
