d04079a5b5d3b1f74d15a4d217a5a32e3fff6d59
joshuanelsondev <joshuanelsondev@gmail.com>
2023-03-12 01:28:05 -0500

    Append mock api json data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import './Home.css';
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { getVideos } from '../../api/fetch';
 import {v1 as generateUniqueID} from "uuid";
 import VideoIndex from "../VideoIndex";
@@ -8,6 +9,7 @@ import VideoIndex from "../VideoIndex";
 export default function Home() {
   // const [error, setError ] = useState(false)
   const [videos, setVideos ] = useState([])
+  const navigate = useNavigate();
 
   const [userSearchInput, setUserSearchInput] = useState("");
   const [searchHistory, setSearchHistory] = useState([]);
@@ -24,14 +26,16 @@ export default function Home() {
     if (!duplicateSearch) {
       setSearchHistory([...searchHistory, {search: userSearchInput, id: generateUniqueID()}]);
     }
-
+    navigate(`/videos`, { state: { videos } });
     //Reset the state
     setUserSearchInput("")
 
     //Make fetch call
-    getVideos(userSearchInput, videoQuantity)
+    getVideos("../../assets/testApi.json", videoQuantity)
       .then(response => {
         setVideos(response.items);
+    console.log(response)
+
       }).catch(error => {
         console.log(error)
       })
@@ -48,7 +52,7 @@ export default function Home() {
           <input type="submit" />
         </form>
         <div>{/* Initial thumbnails/map over api data will go here */}</div>
-        <VideoIndex videos={videos} />
+        {/* <VideoIndex videos={videos} /> */}
       </main>
     </>
   );
--- src/components/VideoIndex.js
+++ src/components/VideoIndex.js
@@ -1,20 +1,27 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 
-export default function VideoIndex({ videos }) {
+export default function VideoIndex() {
     const navigate = useNavigate();
+    const location = useLocation();
+    const videos = location.state;
 
-    const videoThumbnails = videos.map(video => {
+    const [videoThumbnails, setVideoThumbnails] = useState(videos)
+    
+
+    setVideoThumbnails( videos.map(video => {
         const url = video.snippet.thumbnails.high.url;
         const width = video.snippet.thumbnails.high.width;
         const height = video.snippet.thumbnails.high.height;
         const alt = video.snippet.title;
         const id = video.id.videoId;
-
         return(
             <img key={id} onClick={() => handleClick(id, video)} src={url} alt={alt} style={{width: `${width}px`, height: `${height}px`}} />
         )
-    })
+    }))
+
+    
 
     function handleClick(id, video) {
 
