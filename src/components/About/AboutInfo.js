2a3bf86122764958b4219a9f56df57d0534bad59
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-11 22:23:17 -0500

    Adds template of bio section

--- src/components/About/AboutInfo.js
+++ src/components/About/AboutInfo.js
@@ -6,19 +6,22 @@ export const aboutInfo = [
   {
     name: 'Ari Jackson',
     img: ari,
-    description: '',
+    description:
+      'Ari is the Design Lead for this project. Outside of pursuit, Ari is passionate about dog training, education  and coding! Check out his other projects to see more of his work.',
     githubUserName: 'ari-jackson',
   },
   {
     name: 'Joshua Nelson',
     img: josh,
-    description: '',
+    description:
+      'Joshua is the Project Manager for this project. [The rest of Josh\'s bio. "Check out his other projects to see more of his work."]',
     githubUserName: 'joshuanelsondev',
   },
   {
     name: 'SangUn Park',
     img: sang,
-    description: '',
+    description:
+      'SangUn is the Technical Lead for this project. [The rest of Sang\'s bio. "Check out his other projects to see more of his work."]',
     githubUserName: 'SangunPark96',
   },
 ];
