60999ad6e97bdda8396f116f7ff27155a3c30499
joshuanelsondev <joshuanelsondev@gmail.com>
2023-03-13 10:22:26 -0400

    Update fetch call

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -9,11 +9,11 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 // When searching for videos use the <snippet.channelId> after 'relatedToVideoId=' from the JSON: BASE_URL + 'part=snippet&relatedToVideoId=Ks-_Mh1QhMc&type=video&key=[YOUR_API_KEY]'
 
 export function getVideos(keyword, videoQuantity) {
-    // return fetch(`${BASE_URL}${videoQuantity}&q=${keyword}&key=${API_KEY}`).then(response => response.json());
+    return fetch(`${BASE_URL}${videoQuantity}&q=${keyword}&key=${API_KEY}`).then(response => response.json());
 
    
    
     // Temporary mock fetch used when limiting the number of api calls
-    return fetch(keyword).then(response => keyword);
+    // return fetch(keyword).then(response => keyword);
 }
 
