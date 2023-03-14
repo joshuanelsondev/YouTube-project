013f28a96e4edc43eb6e5b470c0e92383fa115ae
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-14 12:36:30 -0400

    Moves searchbar folder into nav folder

--- src/components/Nav/SearchBar/SearchBar.js
+++ src/components/Nav/SearchBar/SearchBar.js
@@ -1,14 +1,13 @@
 import './SearchBar.css';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { getVideos } from '../../api/fetch';
+import { getVideos } from '../../../api/fetch';
 import { v1 as generateUniqueID } from 'uuid';
-import VideoIndex from '../VideoIndex/VideoIndex';
-import testApi from '../../api/testApi.json';
+import VideoIndex from '../../VideoIndex/VideoIndex';
+import testApi from '../../../api/testApi.json';
 import { AiOutlineSearch } from 'react-icons/ai';
 
-export default function SearchBar({ videos, setVideos }) {
-  const [error, setError] = useState(false);
+export default function SearchBar({ setError, setVideos }) {
   const navigate = useNavigate();
   const [userSearchInput, setUserSearchInput] = useState('');
   const [searchHistory, setSearchHistory] = useState([]);
