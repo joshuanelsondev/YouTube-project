85481c914c02b11a4fd0d945043814d4e4b24e79
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-14 13:19:08 -0400

    Makes error modal closable

--- src/components/ErrorMessage/ErrorMessage.js
+++ src/components/ErrorMessage/ErrorMessage.js
@@ -2,14 +2,17 @@ import './ErrorMessage.css';
 import { ReactComponent as Icon } from '../../assets/404-image.svg';
 import { AiOutlineClose } from 'react-icons/ai';
 
-export default function ErrorMessage() {
+export default function ErrorMessage({ setError }) {
   return (
     <div className="error-message-background">
       <div className="error-message">
-        <AiOutlineClose className="x-svg" />
+        <AiOutlineClose className="x-svg" onClick={() => setError(false)} />
         <Icon className="void-svg" />
-        <h1>Oh no! We can't find what you're looking for</h1>
-        <p>Try a differnet search to see more results!</p>
+        <h1>Oh no! Something went wrong</h1>
+        <p>
+          We can't find what you're looking for right now. Try a differnet
+          search to see more results or try again later.
+        </p>
       </div>
     </div>
   );
